480c74ffca631411116de276e2522606f23ff78b
Matheus <mfabryny@minsait.com>
2022-11-04 20:04:12 -0300

    Utilizando lib formidable para receber arquivos com express no Node.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require("formidable")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res, next) => {
+
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ fields, files });
+  });
+
 });
 
 module.exports = router;

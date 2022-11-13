36695eb7b33a74764ad6432c9fbe61a82bb2b58a
Matheus <mfabryny@minsait.com>
2022-11-13 04:43:14 -0300

    Excluindo arquivos do firebase e localmente

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -41,7 +41,41 @@ class DropBoxController {
       return this.listFilesEl.querySelectorAll('.selected');
     }
   
-      initEvents() {
+    removeTask() {
+      let promises = [];
+  
+      this.getSelection().forEach((li) => {
+        let file = JSON.parse(li.dataset.file);
+        let key = li.dataset.key;
+        let formData = new FormData()
+  
+        formData.append('path', file.filepath);
+        formData.append('key', key);
+        promises.push(this.ajax('/file', 'DELETE', formData));
+  
+      });
+      
+      return Promise.all(promises);
+    }
+   
+
+    initEvents() {
+
+      this.btnDelete.addEventListener("click", (e) => {
+        this.removeTask()
+          .then((responses) => {
+  
+            responses.forEach(response => {
+              if (response.fields.key) {
+                this.getFirebaseRef().child
+                (response.fields.key).remove();
+              }
+            })
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
       
       this.btnRename.addEventListener('click', e => {
         
@@ -51,8 +85,6 @@ class DropBoxController {
         let name = prompt("Renomear o arquivo: ", file.name)
 
         if(name){
-          console.log(name)
-          console.log(li.dataset)
           file.name = name;
           this.getFirebaseRef().child(li.dataset.key).set(file);
         } else console.log('erro')
@@ -120,41 +152,61 @@ class DropBoxController {
       this.snackModalEl.style.display = (show) ? 'block' : 'none'
     }
   
+    ajax(
+      url,
+      method = "GET",
+      formData = new FormData(),
+      onprogress = function () {},
+      onloadstart = function () {}
+    ) {
+      return new Promise((resolve, reject) => {
+        let ajax = new XMLHttpRequest();
+  
+        ajax.open(method, url);
+  
+        ajax.onload = (event) => {
+          try {
+            resolve(JSON.parse(ajax.responseText));
+          } catch (e) {
+            reject(e);
+          }
+        };
+  
+        ajax.onerror = (event) => {
+          reject(event);
+        };
+  
+        ajax.upload.onprogress = onprogress;
+  
+        onloadstart();
+  
+        ajax.send(formData);
+      });
+    }
+  
     uploadTask(files) {
       let promises = [];
   
       /* files vem como coleção, tem que converder em array*/
       [...files].forEach( (file) => {
-        promises.push( new Promise((resolve, reject) => {
-          let ajax = new XMLHttpRequest();
+        let formData = new FormData();
+
+        formData.append("input-file", file);
   
-          ajax.open('POST', '/upload')
-  
-          ajax.onload = event => {
-            try {
-              resolve(JSON.parse(ajax.responseText)) /* responseText -> resposta do servidor */
-            } catch (e) {
-              reject(e)
+        promises.push(
+          this.ajax(
+            "/upload",
+            "POST",
+            formData,
+            () => {
+              this.uploadProgress(event, file);
+            },
+            () => {
+              this.startUploadTime = Date.now();
             }
-          }
-  
-          ajax.onerror = event => {
-            reject(event)
-          }
-
-          ajax.upload.onprogress = event => {
-            this.uploadProgress(event, file)
-          }
-  
-          let formData = new FormData() /* Ler os arquivos */
-  
-          formData.append('input-file', file);
-
-          this.startUploadTime = Date.now()
-  
-          ajax.send(formData);
-        }))
-      })
+          )
+        );
+      });
   
       return Promise.all(promises) /* Se for sucesso da resolve, se der problema: reject  */
     }
@@ -404,7 +456,6 @@ class DropBoxController {
             });
 
             let index = [indexStart, indexEnd].sort()
-              console.log(index)
             lis.forEach((el, i) => {
               if(i >= index[0] && i <= index[1]) {
                 el.classList.add('selected');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var formidable = require("formidable")
+var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload', (req, res, next) => {
+router.delete('/file', (req, res) => {
+  const form = formidable({ multiples: true, uploadDir: './uploads' });
+  form.parse(req, (err, fields, files) => {
+    console.log('aaaaaaa',fields.path)
+    let path =  fields.path
+    console.log(path)
+    if (fs.existsSync(path)){
+      console.log('entrou aq')
+      fs.unlink(path, err => {
+        if(err) {   console.log('entrou aq nounlinik ERRO',path)
+          res.status(400).json({
+            err
+          });
+        } else {
+          console.log('entrou aq nounlinik',path)
+          res.json({
+            fields
+          })
+        }
+      });
+    } else {
+      console.log('nao tem path')
+    }
+ 
+  });
+});
 
+router.post('/upload', (req, res, next) => {
+  console.log('upload')
   const form = formidable({ multiples: true, uploadDir: './uploads' });
 
   form.parse(req, (err, fields, files) => {

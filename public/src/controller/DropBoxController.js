26d09cb04963ceee631c7dbaf8a09864669d82bf
Matheus <mfabryny@minsait.com>
2022-11-13 16:52:43 -0300

    Excluindo arquivos do Firebase Storage

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -51,11 +51,21 @@ class DropBoxController {
       this.getSelection().forEach((li) => {
         let file = JSON.parse(li.dataset.file);
         let key = li.dataset.key;
-        let formData = new FormData()
-  
-        formData.append('path', file.filepath);
-        formData.append('key', key);
-        promises.push(this.ajax('/file', 'DELETE', formData));
+        
+        promises.push(new Promise((resolve, reject) =>{
+          let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name)
+          fileRef.delete().then(()=>{
+            resolve({
+              fields: {
+                key: key
+              }
+            })
+          }).catch(error =>{
+            reject(error)
+          })
+        }))
+
+
   
       });
       

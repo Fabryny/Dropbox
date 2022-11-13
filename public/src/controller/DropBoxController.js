041682b96a97a91dfd4d06d8866eb4ca835f3bcc
Matheus <mfabryny@minsait.com>
2022-11-13 03:07:45 -0300

    Renomeando arquivos em conjunto do firebae

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -42,6 +42,23 @@ class DropBoxController {
     }
   
       initEvents() {
+      
+      this.btnRename.addEventListener('click', e => {
+        
+        let li = this.getSelection()[0]
+        let file = JSON.parse(li.dataset.file);
+
+        let name = prompt("Renomear o arquivo: ", file.name)
+
+        if(name){
+          console.log(name)
+          console.log(li.dataset)
+          file.name = name;
+          this.getFirebaseRef().child(li.dataset.key).set(file);
+        } else console.log('erro')
+
+
+      })
 
       this.listFilesEl.addEventListener('selectionchange', e=> {
          
@@ -348,13 +365,14 @@ class DropBoxController {
       let li = document.createElement('li')
 
       li.dataset.key = key
+      li.dataset.file = JSON.stringify(file)
       li.innerHTML = `
       ${this.getFileIconView(file)}
-      <div class="name text-center">${file.originalFilename}</div>`
+      <div class="name text-center">${ file.name ? file.name : file.originalFilename}</div>`
       
       this.initEventsLi(li)
 
-    return  li
+      return  li
     }
   
     readFiles() {
